4e7eca84c79a82e4d14f0c824c996fd458d0f8f7
amazonsx <shixiao@ict.ac.cn>
2015-01-19 12:48:38 +0800

    Modify soft links for qemu-ifup/down files

--- qscript/qemu_if_script_config.sh
+++ qscript/qemu_if_script_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf /etc/qemu-ifup
 rm -rf /etc/qemu-ifdown
-ln -s $HOME/backpack/qscript/qemu-ifup /etc/qemu-ifup
-ln -s $HOME/backpack/qscript/qemu-ifdown /etc/qemu-ifdown
+ln -s $HOME/backpack/frscript/qscript/qemu-ifup /etc/qemu-ifup
+ln -s $HOME/backpack/sfscript/qscript/qemu-ifdown /etc/qemu-ifdown
