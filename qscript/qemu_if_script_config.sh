15cd943c11d1b957af284a9262a1e445df7d587c
amazonsx <shixiao@ict.ac.cn>
2015-01-19 12:52:30 +0800

    Rectify bug: wrong location for qemu-ifdown link in config.sh

--- qscript/qemu_if_script_config.sh
+++ qscript/qemu_if_script_config.sh
@@ -2,4 +2,4 @@
 rm -rf /etc/qemu-ifup
 rm -rf /etc/qemu-ifdown
 ln -s $HOME/backpack/frscripts/qscript/qemu-ifup /etc/qemu-ifup
-ln -s $HOME/backpack/fscripts/qscript/qemu-ifdown /etc/qemu-ifdown
+ln -s $HOME/backpack/frscripts/qscript/qemu-ifdown /etc/qemu-ifdown
